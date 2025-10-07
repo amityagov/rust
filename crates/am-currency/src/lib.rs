742865e422d074550636965cca568d3a0bdb007b
Andrey Mityagov <amityagov@gmail.com>
2025-10-07 21:07:19 +0300

    am-currency crate

--- crates/am-currency/src/lib.rs
+++ crates/am-currency/src/lib.rs
@@ -1,14 +1,112 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+
+#[cfg(feature = "sqlx")]
+mod sqlx_support;
+
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use std::fmt::{Display, Formatter};
+use std::str::FromStr;
+use strum::IntoEnumIterator;
+use strum_macros::{Display, EnumIter, EnumString};
+
+pub type FiatCurrency = iso_currency::Currency;
+
+#[derive(Debug, Display, EnumString, EnumIter, Serialize, Deserialize)]
+#[strum(serialize_all = "UPPERCASE")]
+pub enum CryptoCurrency {
+    #[strum(to_string = "USDC_SOL")]
+    UsdcSol,
+
+    #[strum(to_string = "BTC")]
+    Btc,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug)]
+pub enum Currency {
+    Fiat(FiatCurrency),
+    Crypto(CryptoCurrency),
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Display for Currency {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Currency::Fiat(fiat) => write!(f, "{}", fiat.code()),
+            Currency::Crypto(crypto) => write!(f, "{crypto}"),
+        }
+    }
+}
+impl Currency {
+    pub fn all() -> impl Iterator<Item = Currency> {
+        let fiat = FiatCurrency::iter().map(Currency::Fiat);
+        let crypto = CryptoCurrency::iter().map(Currency::Crypto);
+        fiat.chain(crypto)
+    }
+
+    pub fn all_fiat() -> impl Iterator<Item = Currency> {
+        FiatCurrency::iter().map(Currency::Fiat)
+    }
+
+    pub fn all_crypto() -> impl Iterator<Item = Currency> {
+        CryptoCurrency::iter().map(Currency::Crypto)
+    }
+
+    pub fn is_fiat(&self) -> bool {
+        matches!(self, Currency::Fiat(_))
+    }
+
+    pub fn is_crypto(&self) -> bool {
+        matches!(self, Currency::Crypto(_))
+    }
+
+    pub fn fiat(iso_currency: FiatCurrency) -> Self {
+        Currency::Fiat(iso_currency)
+    }
+
+    pub fn crypto(crypto_currency: CryptoCurrency) -> Self {
+        Currency::Crypto(crypto_currency)
+    }
+}
+
+impl From<FiatCurrency> for Currency {
+    fn from(value: FiatCurrency) -> Self {
+        Currency::Fiat(value)
+    }
+}
+
+impl From<CryptoCurrency> for Currency {
+    fn from(value: CryptoCurrency) -> Self {
+        Currency::Crypto(value)
+    }
+}
+
+impl Serialize for Currency {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            Currency::Fiat(fiat) => serializer.serialize_str(fiat.code()),
+            Currency::Crypto(crypto) => serializer.serialize_str(&crypto.to_string()),
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for Currency {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+
+        // Try to parse as fiat
+        if let Ok(cur) = FiatCurrency::from_str(&s) {
+            return Ok(Currency::Fiat(cur));
+        }
+
+        // Try to parse as crypto
+        if let Ok(crypto) = CryptoCurrency::from_str(&s) {
+            return Ok(Currency::Crypto(crypto));
+        }
+
+        Err(serde::de::Error::custom(format!("Unknown currency: {s}")))
     }
 }
---
+++ crates/am-currency/src/sqlx_support.rs
@@ -0,0 +1,100 @@
+use std::str::FromStr;
+use sqlx::Database;
+use sqlx::encode::IsNull;
+use sqlx::error::BoxDynError;
+use crate::{CryptoCurrency, Currency};
+
+impl<'r, T> sqlx::Decode<'r, T> for CryptoCurrency
+where
+    T: Database,
+    String: sqlx::Decode<'r, T>,
+{
+    fn decode(
+        value: <T as sqlx::Database>::ValueRef<'r>,
+    ) -> Result<Self, sqlx::error::BoxDynError> {
+        let s = <String as sqlx::Decode<T>>::decode(value)?;
+        CryptoCurrency::from_str(&s).map_err(|_| format!("Invalid CryptoCurrency: {s}").into())
+    }
+}
+
+impl<'q, T> sqlx::Encode<'q, T> for CryptoCurrency
+where
+    T: Database,
+    String: sqlx::Encode<'q, T>,
+{
+    fn encode_by_ref(
+        &self,
+        buf: &mut <T as sqlx::Database>::ArgumentBuffer<'q>,
+    ) -> Result<IsNull, BoxDynError> {
+        <String as sqlx::Encode<T>>::encode_by_ref(&self.to_string(), buf)
+    }
+
+    fn produces(&self) -> Option<<T as sqlx::Database>::TypeInfo> {
+        <String as sqlx::Encode<T>>::produces(&self.to_string())
+    }
+
+    fn size_hint(&self) -> usize {
+        <String as sqlx::Encode<T>>::size_hint(&self.to_string())
+    }
+}
+
+impl<T: Database> sqlx::Type<T> for CryptoCurrency
+where
+    String: sqlx::Type<T>,
+{
+    fn type_info() -> <T as sqlx::Database>::TypeInfo {
+        <String as sqlx::Type<T>>::type_info()
+    }
+}
+
+impl<'r, T> sqlx::Decode<'r, T> for Currency
+where
+    T: Database,
+    String: sqlx::Decode<'r, T>,
+{
+    fn decode(value: <T as Database>::ValueRef<'r>) -> Result<Self, sqlx::error::BoxDynError> {
+        let s = <String as sqlx::Decode<T>>::decode(value)?;
+
+        if let Ok(c) = iso_currency::Currency::from_str(&s) {
+            return Ok(Currency::Fiat(c));
+        } else if let Ok(c) = CryptoCurrency::from_str(&s) {
+            return Ok(Currency::Crypto(c));
+        }
+        Err(format!("Invalid currency: {s}").into())
+    }
+}
+
+impl<'q, T> sqlx::Encode<'q, T> for Currency
+where
+    T: Database,
+    String: sqlx::Encode<'q, T>,
+{
+    fn encode_by_ref(
+        &self,
+        buf: &mut <T as Database>::ArgumentBuffer<'q>,
+    ) -> Result<sqlx::encode::IsNull, BoxDynError> {
+        let value = self.to_string();
+        <String as sqlx::Encode<T>>::encode_by_ref(&value, buf)
+    }
+
+    fn produces(&self) -> Option<<T as sqlx::Database>::TypeInfo> {
+        let value = self.to_string();
+
+        <String as sqlx::Encode<T>>::produces(&value)
+    }
+
+    fn size_hint(&self) -> usize {
+        let value = self.to_string();
+        <String as sqlx::Encode<T>>::size_hint(&value)
+    }
+}
+
+impl<T> sqlx::Type<T> for Currency
+where
+    T: Database,
+    String: sqlx::Type<T>,
+{
+    fn type_info() -> <T as Database>::TypeInfo {
+        <String as sqlx::Type<T>>::type_info()
+    }
+}
